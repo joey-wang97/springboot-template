2259fe7701b373efee111dbbf22619b35bc63275
tianyu <545593725@qq.com>
2020-08-07 17:56:12 +0800

    add shutdown hook

--- src/main/java/wang/joye/springboottemplate/SpringbootTemplateApplication.java
+++ src/main/java/wang/joye/springboottemplate/SpringbootTemplateApplication.java
@@ -1,12 +1,16 @@
 package wang.joye.springboottemplate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 @MapperScan("wang.joye.springboottemplate.mapper")
 public class SpringbootTemplateApplication extends SpringBootServletInitializer {
 
@@ -19,4 +23,14 @@ public class SpringbootTemplateApplication extends SpringBootServletInitializer 
         return application.sources(SpringbootTemplateApplication.class);
     }
 
+    @PostConstruct
+    private void init() {
+        /*
+         * 注册shutdown钩子，当应用程序正常关闭时，会调用这里的方法
+         * kill -9时，不会触发这段代码
+         */
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("stop application successfully");
+        }));
+    }
 }
